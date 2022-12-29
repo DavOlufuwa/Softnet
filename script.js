f453727236b1f30c44bcd911259710b7082b51b7
DavOlufuwa <davolufuwa@gmail.com>
2022-12-29 23:50:29 +0100

    increased sectional font sizes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const mobileButton = document.querySelector(".mobile-button");
 const menuNav = document.querySelector(".main-nav")
 const body= document.querySelector("body")
 let beans = true;
+const navLinks = document.querySelectorAll(".nav-link")
 const hamburger = document.getElementById("hamburger");
 
 mobileButton.addEventListener("click",()=>{
@@ -10,4 +11,14 @@ mobileButton.addEventListener("click",()=>{
     menuNav.classList.toggle("opened")
     body.classList.toggle("choked")
     beans ? hamburger.src = "./images/Hamburger-open.svg" :hamburger.src = "./images/Hamburger-close.svg"
+})
+
+
+navLinks.forEach((link)=>{
+    link.addEventListener("click",()=>{
+        beans = !beans
+        menuNav.classList.toggle("opened")
+        body.classList.toggle("choked")
+        beans ? hamburger.src = "./images/Hamburger-open.svg" :hamburger.src = "./images/Hamburger-close.svg"
+    })
 })
